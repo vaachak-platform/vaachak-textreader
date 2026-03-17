49b9ef4aa7c206458ea1346477189b2090919a3c
Piyush Daiya <piyushdaiya@mac-mini.tail1f3687.ts.net>
2026-03-17 12:14:02 -0400

    reduced number of English voices to US and UK only.

--- app/src/main/java/org/vaachak/textreader/MainActivity.kt
+++ app/src/main/java/org/vaachak/textreader/MainActivity.kt
@@ -1,3 +1,29 @@
+/*
+ *
+ *  *
+ *  *  *  Copyright (c) 2026 Piyush Daiya
+ *  *  *  *
+ *  *  *  * Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  *  *  * of this software and associated documentation files (the "Software"), to deal
+ *  *  *  * in the Software without restriction, including without limitation the rights
+ *  *  *  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  *  *  * copies of the Software, and to permit persons to whom the Software is
+ *  *  *  * furnished to do so, subject to the following conditions:
+ *  *  *  *
+ *  *  *  * The above copyright notice and this permission notice shall be included in all
+ *  *  *  * copies or substantial portions of the Software.
+ *  *  *  *
+ *  *  *  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  *  *  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  *  *  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  *  *  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  *  *  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  *  *  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  *  *  * SOFTWARE.
+ *  *
+ *
+ */
+
 package org.vaachak.textreader
 
 import android.content.ClipboardManager
@@ -364,9 +390,18 @@ fun TtsSettingsDialog(
     LaunchedEffect(currentLocale) {
         try {
             val allVoices = ttsEngine.voices ?: emptySet()
-            val filteredVoices = allVoices
-                .filter { it.locale.language == currentLocale.language }
-                .sortedBy { it.name }
+
+            val filteredVoices = allVoices.filter { voice ->
+                val voiceLocale = voice.locale
+
+                // If the current language is English, only show US and UK variants
+                if (voiceLocale.language == "en") {
+                    voiceLocale.country == "US" || voiceLocale.country == "GB"
+                } else {
+                    // For all other languages (Hindi, Gujarati, etc.), show all variants
+                    voiceLocale.language == currentLocale.language
+                }
+            }.sortedBy { it.name }
 
             setAvailableVoices(filteredVoices)
             setSelectedVoice(ttsEngine.voice)
